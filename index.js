ff1573ecf38a53907b0a88520ea04981393fe258
a_tretyakov <artem.tretyakov.91@inbox.ru>
2017-09-04 13:31:22 +0300

    tick

---
+++ index.js
@@ -0,0 +1,6 @@
+ReactDOM.render(
+    <h1 id='title'>
+        Hello React
+    </h1>,    // элемент, который мы хотим создать
+    document.getElementById("container")    // где мы этот элемент хотим создать
+)
